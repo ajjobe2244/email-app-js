05efe459bb2cc947126ac8bf6e29502c80b95add
Ali Jobe <ali.jobe@virginpulse.com>
2019-08-22 09:18:34 -0400

    Redux thunk

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
+import { FETCH_USER } from '../actions/types';
+
 export default function(state = {}, action) {
-    switch (action.type) {
-        default:
-            return state;
-    }
+	console.log(action);
+	switch (action.type) {
+        case FETCH_USER:
+            
+		default:
+			return state;
+	}
 }
